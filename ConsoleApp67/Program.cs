9ee04167686a545e51090ba46bb43f36ce5baea5
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-08-09 17:44:00 +0330

    Add files via upload

---
+++ ConsoleApp67/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp67
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cubnumber = cub(5);
+            Console.WriteLine(cubnumber);
+            Console.ReadLine();
+        }
+
+        static int cub(int num)
+        {
+            int result = num * num * num;
+            return result;
+        }
+
+    }
+}
